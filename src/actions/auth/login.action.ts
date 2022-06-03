63329a532c01be87616f39a24d1d522a2699e300
Miguel <28miguelangelo@gmail.com>
2022-06-03 09:03:29 +0100

    Added login

--- src/actions/auth/login.action.ts
+++ src/actions/auth/login.action.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
+import attemptLogin from "../../security/services/attempt-login.service";
 
-const action = async (req: Request, res: Response) => {
-  return res.json({ login: "login" });
+const action = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { email, password } = req.body;
+    const token = await attemptLogin(email, password);
+
+    return res.status(200).json({ token });
+  } catch (e) {
+    return res.json(401).json({
+      code: 401,
+      error: "Unauthorized / Bad credentials",
+      message: e.message,
+    });
+  }
 };
 
 export default action;
--- src/actions/auth/register.action.ts
+++ src/actions/auth/register.action.ts
@@ -9,17 +9,17 @@ import register from "../../security/services/register.service";
  * **Status 201 returns** - the status 201 and a json with an object User filled with name, email, password, dateCreated and dateEdited
  * **Status 400 returns** - the status 400 and a json with the code: 400, error which is a string and the message which is error.message
  */
-const action = async (req: Request, res: Response) => {
+const action = async (req: Request, res: Response): Promise<Response> => {
   try {
     const { name, email, password } = req.body;
     const token = await register(name, email, password);
 
     return res.status(201).json({ token });
-  } catch (error) {
+  } catch (e) {
     return res.status(400).json({
       code: 400,
       error: "error while creating user",
-      message: error.message,
+      message: e.message,
     });
   }
 };
