7e38e6d9e726f2c103863b9397155ca98eaaa28e
gieto28 <28miguelangelo@gmail.com>
2022-07-24 15:38:32 +0100

    fixed merge conflitcts

--- src/actions/comments/create-comment.Action.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-const action = async (req: Request, res: Response) => {
-  return res.json({ createComment: "create comment" });
-};
-
-export default action;
