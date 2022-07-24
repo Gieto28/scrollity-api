f875d113d46cf0a74128b8d8bb625519de289f61
gieto28 <28miguelangelo@gmail.com>
2022-07-24 17:20:50 +0100

    fixed naming for ts compiler

--- src/actions/comment/index.ts
+++ src/actions/comment/index.ts
@@ -1,5 +1,5 @@
 import getCommentAction from "./get-comments.action";
-import createCommentAction from "./create-comment.action";
+import createCommentAction from "./create-comments.action";
 import getAllCommentsAction from "./get-all-comments.action";
 import checkCommentVotesAction from "./check-comment-votes.action";
 import handleCommentVoteAction from "./handle-comment-vote.action";
