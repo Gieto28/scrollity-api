8be3b9355eea42740929c8e4b788e25a2bb6eab0
gieto28 <28miguelangelo@gmail.com>
2022-07-24 17:49:09 +0100

    added appDatasource back

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import app from "./app";
 
 const PORT = 3003;
 
-// AppDataSource.initialize()
-//   .then(async () => {
-// start express server
-app.listen(PORT, () =>
-  console.log(`\u{1F525} Server starting on port ${PORT}`)
-);
-// })
-// .catch((error) => console.log("error on file Index.ts", error));
+AppDataSource.initialize()
+  .then(async () => {
+    // start express server
+    app.listen(PORT, () =>
+      console.log(`\u{1F525} Server starting on port ${PORT}`)
+    );
+  })
+  .catch((error) => console.log("error on file Index.ts", error));
