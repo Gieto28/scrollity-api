26c3bf4093474b433d93120b28869c2767c3f761
gieto28 <28miguelangelo@gmail.com>
2022-07-24 17:05:38 +0100

    fixing ts compile

--- src/actions/post/index.ts
+++ src/actions/post/index.ts
@@ -1,6 +1,6 @@
 import createPostsAction from "./create-post.action";
 import handleVoteAction from "./handle-vote.action";
-import getAllPostsAction from "./get-all-posts.action";
+import getAllPostsAction from "./get-all-post.action";
 import getPostAction from "./get-post.action";
 import checkPostVotesAction from "./check-post-votes.action";
 import getPostByTitle from "./get-post-by-title.action";
