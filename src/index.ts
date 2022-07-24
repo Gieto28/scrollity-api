debe2a311f4e71d18912fc3cd8131954c9aa03fa
gieto28 <28miguelangelo@gmail.com>
2022-07-24 17:29:21 +0100

    changed syntax for PORT again..

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ require("dotenv").config();
 import { AppDataSource } from "./data-source";
 import app from "./app";
 
+const PORT = 3003;
+
 AppDataSource.initialize()
   .then(async () => {
     // start express server
-    app.listen(process.env.PORT || 3003, () =>
-      console.log(
-        `\u{1F525} Server starting on port ${process.env.PORT || 3003}`
-      )
+    app.listen(PORT, () =>
+      console.log(`\u{1F525} Server starting on port ${PORT}`)
     );
   })
   .catch((error) => console.log("error on file Index.ts", error));
